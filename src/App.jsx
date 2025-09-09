7da9fdfe18f94effa3a91016351f2112a4755867
alomatee@yahoo.com <alomatee@yahoo.com>
2025-09-09 06:14:24 +0100

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DoorControl from './controller';
-import Email from './Email';
-import LoginPage from './LoginPage'
-
-
-
+import { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import DoorControl from "./controller";
+import Email from "./Email";
+import LoginPage from "./LoginPage";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/controls" element={<DoorControl />} />
           <Route path="/" element={<LoginPage />} />
           <Route path="/update-email" element={<Email />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
 
 export default App;
-
